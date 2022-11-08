507bb2415801c3f2aa6165e9ab13de0d8dc2cde2
MC34X <117679539+MC34X@users.noreply.github.com>
2022-11-08 00:09:20 -0800

    Add files via upload

---
+++ leakiller/config.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from lea.ttypes import ApplicationType
+import re
+
+class Config(object):
+    LINE_HOST_DOMAIN            = 'https://legy-jp.line.naver.jp'
+    LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
+    LINE_TIMELINE_API           = 'https://legy-jp.line.naver.jp/mh/api'
+    LINE_TIMELINE_MH            = 'https://legy-jp.line.naver.jp/mh'
+    LINE_LOGIN_QUERY_PATH       = '/api/v4p/rs'
+    LINE_AUTH_QUERY_PATH        = '/api/v4/TalkService.do'
+    LINE_API_QUERY_PATH_FIR     = '/S4'
+    LINE_POLL_QUERY_PATH_FIR    = '/P4'
+    LINE_CALL_QUERY_PATH        = '/V4'
+    LINE_CERTIFICATE_PATH       = '/Q'
+    LINE_CHAN_QUERY_PATH        = '/CH4'
+
+    CHANNEL_ID = {
+        'LINE_TIMELINE': '1341209850',
+        'LINE_WEBTOON': '1401600689',
+        'LINE_TODAY': '1518712866',
+        'LINE_STORE': '1376922440'
+    }
+    CARRIER     = '51089, 1-0'
+    SYSTEM_NAME = 'SELFBOT-VERSION'
+    IP_ADDR     = '8.8.8.8'
+    EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    def __init__(self):
+        self.APP_NAME = 'CHANNELCP\t2.9.1\tAndroid OS\t5.1.1'
+        self.USER_AGENT = 'LLA/2.9.1 SM-J320G 5.1.1'
